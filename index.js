428e9d3d02224bda17cfc7dbb9b23fc6b7042495
TURNEDz <thanagorn@iconframework.com>
2020-06-02 18:31:35 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const port = process.env.PORT || 4000;
+
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.use('/webhook', require('./routes/webhook.js'));
+
+app.get('/', (req, res) => {
+  res.send("<h1>Welcome to webhook-dialogflow</h1>");
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running at port: ${port}`);
+});
